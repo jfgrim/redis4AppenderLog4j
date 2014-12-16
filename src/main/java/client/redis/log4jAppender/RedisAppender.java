e71a6aa812eaa88f13b54d956e7e143e9c7c0622
Jef GRIMAULT <jean-francois.grimault@pmu.fr>
2014-12-16 11:54:21 +0100

    [JFG][Fault-Tolerance] refactoring

--- src/main/java/client/redis/log4jAppender/RedisAppender.java
+++ src/main/java/client/redis/log4jAppender/RedisAppender.java
@@ -1,7 +1,7 @@
 /**
 * This file is part of log4j-redis-appender
 *
- Copyright 2012 Ryan Tenney
+ Copyright 2012 Jef GRIMAULT
 
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
